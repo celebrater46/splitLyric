4e135c8b36a604a83c8e1beb0dbb2d619a713e02
celebrater <tkgm45nbchk@gmail.com>
2023-03-10 08:23:31 +0900

    first commit

---
+++ splitLyric.ps1
@@ -0,0 +1,25 @@
+[string] $lyric = "hatarakitakunainyonnnyonnmyohoooo"
+
+function isVowel($lyric){
+    if(($zero -eq "a") -Or ($zero -eq "i") -Or ($zero -eq "u") -Or ($zero -eq "e") -Or ($zero -eq "o")){
+        return $TRUE
+    } else {
+        return $FALSE
+    }
+}
+
+function trimOneChar($lyric){
+    if($lyric.Length -eq 1) {
+        return @{char=$lyric; remain=""}
+    }
+
+    $zero = $lyric.Substring(0, 1)
+    if(($zero -eq "a") -Or ($zero -eq "i") -Or ($zero -eq "u") -Or ($zero -eq "e") -Or ($zero -eq "o")){
+        return @{char=$zero; remain=$lyric.Substring(1)}
+    } elseif(($zero -eq "k") -Or ($zero -eq "s") -Or ($zero -eq "t") -Or ($zero -eq "n") -Or ($zero -eq "h") -Or ($zero -eq "m") -Or ($zero -eq "y") -Or ($zero -eq "r") -Or ($zero -eq "w") -Or ($zero -eq "g") -Or ($zero -eq "z") -Or ($zero -eq "d") -Or ($zero -eq "b") -Or ($zero -eq "p") -Or ($zero -eq "x") -Or ($zero -eq "l") -Or ($zero -eq "c") -Or ($zero -eq "v") -Or ($zero -eq "q") -Or ($zero -eq "f") -Or ($zero -eq "j")){
+        $one = $lyric.Substring(1, 1)
+        if($zero -eq $one){
+            return @{char=$zero; remain=$lyric.Substring(1)}
+        }
+    }
+}
